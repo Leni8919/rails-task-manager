fd091bf8a4719efe1f8000bd49de536d921b33dd
Lena Maglo <maglo.lena@gmail.com>
2019-08-13 16:49:26 +0200

    this is my todo

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
 
   def index
     @tasks = Task.all
@@ -11,6 +13,22 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
+    redirect_to tasks_path
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
     redirect_to tasks_path
   end
 
